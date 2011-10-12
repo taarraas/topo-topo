5405ebcdc1c001d4ba60697ad53726d9246f3b78
Vladyslav Simonenko <simonenko.ua@gmail.com>
2011-10-12 19:58:11 +0300

    Dirty test generation routine

--- main.cpp
+++ main.cpp
@@ -6,11 +6,14 @@
  */
 
 #include <cstdlib>
+#include <cstdio>
+#include <fstream>
 #include "Core.h"
 #include "Scene.h"
 #include "SceneFactory.h"
 #include "OpenGlRenderer.h"
 #include "TopoView.h"
+#include "TriangleStorageImpl.h"
 #include "Renderer.h"
 #include "Geometry.h"
 
@@ -18,24 +21,27 @@
  * 
  */
 
-#include "Cylinder.h"
-#include "TriangleStorageImpl.h"
-void testCylinder() {
-    TriangleStoragePtr storage = new SimpleTriangleStorage();
-    Point a = Point(0, 0, 0);	
-    Point b = Point(0, 10, 0);	
-    float r = 2;
-    ShapePtr shape = new Cylinder(storage, a, b, r, 10, 10);
-    Point q = Point(2, 2, 2);
-    Point w = Point(0, 2, 1);
-    LOG_INFO << shape->getIntersection(q, w) << std::endl;
+void generateGrid(std::string fileName, int size) {
+    std::ofstream genTest (fileName.c_str());
+    for (int i = 0; i < size; ++i) {
+	for (int j = 0; j < size; ++j) {
+	    for (int k = 0; k < size; ++k) {
+		for (int vi = i; vi < i+2; ++vi) {
+		    for (int vj = j; vj < j+2; ++vj) {
+			for (int vk = k; vk < k+2; ++vk) if (vi+vj+vk-i-j-k == 1) {			    
+			    genTest << "A " << i << ' ' << j << ' ' << k << " " << vi << ' ' << vj << ' ' << vk << std::endl;
+			}
+		    }
+		}
+	    }
+	}	
+    }
+    genTest.close();
 }
 
-int main(int argc, char** argv) {
+void run(std::string mapFileName) {
     try {
-		// testCylinder();
-		// return 0;
-        ScenePtr scene = SceneFactory::createScene("sample.map");
+        ScenePtr scene = SceneFactory::createScene(mapFileName);
         std::vector<Triangle> triangles;
         scene->getTriangles(triangles);
         
@@ -45,8 +51,14 @@ int main(int argc, char** argv) {
         renderer->run(view);
     } catch (Exception e) {
         LOG_INFO << e.message;
-        return -1;
+	std::exit(-1);
     }
+}
+
+int main(int argc, char** argv) {
+    //    generateGrid("gen.map", 2);
+    //    run("gen.map");    
+    run("sample.map");
     return 0;
 }
 
